f58ddc5a3ebd15a1ded171252eccbaf84342dee2
aocneanu <aocneanu@gmail.com>
2017-06-21 08:44:01 +0000

    Apply fixes from StyleCI

--- src/app/Classes/GroupPermissionStructure.php
+++ src/app/Classes/GroupPermissionStructure.php
@@ -7,30 +7,30 @@ use LaravelEnso\Helpers\Classes\Object;
 
 class GroupPermissionStructure
 {
-	private $structure;
+    private $structure;
 
-	public function __construct(Collection $groups)
-	{
-		$this->structure = $this->build($groups);
-	}
+    public function __construct(Collection $groups)
+    {
+        $this->structure = $this->build($groups);
+    }
 
-	public function get()
-	{
-		return $this->structure;
-	}
+    public function get()
+    {
+        return $this->structure;
+    }
 
     private function build($groups)
     {
         $currentLevel = new Object();
 
-        $this->getStartingLabels($groups)->each(function($label) use ($currentLevel, $groups) {
-        	$childGroups = $this->getChildGroups($label, $groups);
+        $this->getStartingLabels($groups)->each(function ($label) use ($currentLevel, $groups) {
+            $childGroups = $this->getChildGroups($label, $groups);
 
-        	if ($childGroups->count() === 1) {
-        		return $currentLevel->$label = $childGroups->first()->permissions;
-        	}
+            if ($childGroups->count() === 1) {
+                return $currentLevel->$label = $childGroups->first()->permissions;
+            }
 
-    		$currentLevel->$label = $this->build($this->getTrimmedNameGroups($childGroups));
+            $currentLevel->$label = $this->build($this->getTrimmedNameGroups($childGroups));
         });
 
         return $currentLevel;
@@ -38,11 +38,11 @@ class GroupPermissionStructure
 
     private function getStartingLabels($groups)
     {
-    	$labels = collect();
+        $labels = collect();
 
-    	$groups->each(function($group) use ($labels) {
-        	$groupLabels = collect(explode('.', $group->name));
-        	$labels->push($groupLabels->first());
+        $groups->each(function ($group) use ($labels) {
+            $groupLabels = collect(explode('.', $group->name));
+            $labels->push($groupLabels->first());
         });
 
         return $labels->unique()->values();
@@ -50,24 +50,24 @@ class GroupPermissionStructure
 
     private function getChildGroups($label, $groups)
     {
-    	return $groups->filter(function($group) use($label) {
-    		return $label === current(explode('.', $group->name));
-    	})->values();
+        return $groups->filter(function ($group) use ($label) {
+            return $label === current(explode('.', $group->name));
+        })->values();
     }
 
     private function getTrimmedNameGroups($groups)
     {
-    	return $groups->map(function($group) {
-    		return $this->trimGroupName($group);
-    	});
+        return $groups->map(function ($group) {
+            return $this->trimGroupName($group);
+        });
     }
 
     private function trimGroupName($group)
     {
-    	$labels = collect(explode('.', $group->name));
-		$labels->shift();
-		$group->name = $labels->implode('.');
+        $labels = collect(explode('.', $group->name));
+        $labels->shift();
+        $group->name = $labels->implode('.');
 
-		return $group;
+        return $group;
     }
 }
