18ddf00f171820f0fc3a4f0d1961b63c3bfb1506
mishu <mishu@xtelecom.ro>
2017-06-27 23:43:53 +0300

    updated forbidden action message

--- src/app/Http/Middleware/VerifyRouteAccess.php
+++ src/app/Http/Middleware/VerifyRouteAccess.php
@@ -11,7 +11,7 @@ class VerifyRouteAccess
         if (!$request->user()->can('access-route', $request->route()->getName())) {
             \Log::warning('The user having id [ '.$request->user()->id.' ] is not allowed on route [ '.$request->route()->getName().' ] ');
 
-            throw new \EnsoException(__('You are not authorized here'), 'error', [], 403);
+            throw new \EnsoException(__('You are not authorized to perform this action'), 'error', [], 403);
         }
 
         return $next($request);
