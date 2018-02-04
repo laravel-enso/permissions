bc55781a20d7749bc73eb9459357a876c7024246
mishu <mishu@xtelecom.ro>
2018-02-04 09:59:55 +0200

    removes Log

--- src/app/Http/Middleware/VerifyRouteAccess.php
+++ src/app/Http/Middleware/VerifyRouteAccess.php
@@ -10,10 +10,6 @@ class VerifyRouteAccess
     public function handle($request, Closure $next)
     {
         if (!$request->user()->can('access-route', $request->route()->getName())) {
-            \Log::warning(
-                'The user having id [ '.$request->user()->id.' ] is not allowed on route [ '.$request->route()->getName().' ] '
-                );
-
             throw new AuthorizationException(__(config('enso.labels.notAuthorized')));
         }
 
