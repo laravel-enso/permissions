2e5852f7d05e11f5af8eb6fecc9fed8be21942ee
mishu <mishu@xtelecom.ro>
2018-03-31 15:35:49 +0300

    updates the route helper usage

--- src/resources/assets/js/pages/system/permissions/Index.vue
+++ src/resources/assets/js/pages/system/permissions/Index.vue
@@ -2,14 +2,14 @@
 
     <vue-table class="box"
         :path="path"
-        :custom-render="customRender"
-        id="permissions">
+        id="permissionGroups">
     </vue-table>
 
 </template>
 
 <script>
 
+import { mapGetters } from 'vuex';
 import VueTable from '../../../components/enso/vuedatatable/VueTable.vue';
 
 export default {
@@ -17,21 +17,8 @@ export default {
 
     data() {
         return {
-            path: route('system.permissions.initTable', [], false),
+            path: route('system.permissionGroups.initTable'),
         };
-    },
-
-    methods: {
-        customRender(row, column) {
-            switch (column.name) {
-            case 'type':
-                return row.isRead ? `<span class="tag is-table-tag is-success">${this.__(row[column.name])}</span>`
-                    : `<span class="tag is-table-tag is-warning">${this.__(row[column.name])}</span>`;
-            default:
-                this.$toastr.warning(`render for column ${column.name} is not defined.`);
-                return this.__(row[column.name]);
-            }
-        },
     },
 };
 
