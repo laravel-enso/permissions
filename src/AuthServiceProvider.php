9ac7da6391c8675e713ec2bf5e5e45bb10289bdb
Adrian Ocneanu <aocneanu@gmail.com>
2018-10-21 11:59:20 +0000

    Apply fixes from StyleCI

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -9,7 +9,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         \Gate::define('access-route', function ($user, $route) {
-            return !is_null(
+            return ! is_null(
                 $user->role->permissions()
                     ->whereName($route)
                     ->first()
