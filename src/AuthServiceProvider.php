2392ff3b28129e18ee61e2b6b5298d7b20b4b37d
mishu <mishu@xtelecom.ro>
2019-07-06 20:01:02 +0300

    refactors facade use

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -2,13 +2,14 @@
 
 namespace LaravelEnso\Permissions;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        \Gate::define('access-route', function ($user, $route) {
+        Gate::define('access-route', function ($user, $route) {
             return $user->role->permissions()
                 ->whereName($route)
                 ->first() !== null;
