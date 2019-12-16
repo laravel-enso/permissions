c1993d6a1c94d259e4d440d31f7e81775c49575a
Mihai <mihai.ocneanu@gmail.com>
2019-12-16 13:05:39 +0200

    reverted routes split

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,5 +4,13 @@ Route::middleware(['web', 'auth', 'core'])
     ->prefix('api/system/permissions')->as('system.permissions.')
     ->namespace('LaravelEnso\Permissions\app\Http\Controllers')
     ->group(function () {
-        require 'app/permissions.php';
+        Route::get('create', 'Create')->name('create');
+        Route::post('', 'Store')->name('store');
+        Route::get('{permission}/edit', 'Edit')->name('edit');
+        Route::patch('{permission}', 'Update')->name('update');
+        Route::delete('{permission}', 'Destroy')->name('destroy');
+
+        Route::get('initTable', 'InitTable')->name('initTable');
+        Route::get('tableData', 'TableData')->name('tableData');
+        Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
     });
