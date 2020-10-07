0cbee8047fa368c6af422d7ac5411ab10de4231b
Mihai <mihai.ocneanu@gmail.com>
2020-10-07 17:32:02 +0300

    updates routes

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use LaravelEnso\Permissions\Http\Controllers\Create;
+use LaravelEnso\Permissions\Http\Controllers\Destroy;
+use LaravelEnso\Permissions\Http\Controllers\Edit;
+use LaravelEnso\Permissions\Http\Controllers\ExportExcel;
+use LaravelEnso\Permissions\Http\Controllers\InitTable;
+use LaravelEnso\Permissions\Http\Controllers\Store;
+use LaravelEnso\Permissions\Http\Controllers\TableData;
+use LaravelEnso\Permissions\Http\Controllers\Update;
 
 Route::middleware(['api', 'auth', 'core'])
     ->prefix('api/system/permissions')->as('system.permissions.')
-    ->namespace('LaravelEnso\Permissions\Http\Controllers')
     ->group(function () {
-        Route::get('create', 'Create')->name('create');
-        Route::post('', 'Store')->name('store');
-        Route::get('{permission}/edit', 'Edit')->name('edit');
-        Route::patch('{permission}', 'Update')->name('update');
-        Route::delete('{permission}', 'Destroy')->name('destroy');
+        Route::get('create', Create::class)->name('create');
+        Route::post('', Store::class)->name('store');
+        Route::get('{permission}/edit', Edit::class)->name('edit');
+        Route::patch('{permission}', Update::class)->name('update');
+        Route::delete('{permission}', Destroy::class)->name('destroy');
 
-        Route::get('initTable', 'InitTable')->name('initTable');
-        Route::get('tableData', 'TableData')->name('tableData');
-        Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
+        Route::get('initTable', InitTable::class)->name('initTable');
+        Route::get('tableData', TableData::class)->name('tableData');
+        Route::get('exportExcel', ExportExcel::class)->name('exportExcel');
     });
